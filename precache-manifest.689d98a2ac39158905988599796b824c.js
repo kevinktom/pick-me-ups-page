e702e99990b914063c84e3c2322ae536ed455edc
Kevin Tom <kevinktom@gmail.com>
2020-12-09 18:02:56 -0500

    Initial Commit

---
+++ precache-manifest.689d98a2ac39158905988599796b824c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aeeae1bbf992abd001863683d041f8a",
+    "url": "/Pick-me-ups/index.html"
+  },
+  {
+    "revision": "223259e177ef8176c928",
+    "url": "/Pick-me-ups/static/css/main.4f1219a7.chunk.css"
+  },
+  {
+    "revision": "ab2aa899f174ae2d69be",
+    "url": "/Pick-me-ups/static/js/2.a77a8f82.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Pick-me-ups/static/js/2.a77a8f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d886116177e5b1c2bc9f",
+    "url": "/Pick-me-ups/static/js/3.33d14f80.chunk.js"
+  },
+  {
+    "revision": "223259e177ef8176c928",
+    "url": "/Pick-me-ups/static/js/main.3c39af92.chunk.js"
+  },
+  {
+    "revision": "a6db0b47137cff09cd74",
+    "url": "/Pick-me-ups/static/js/runtime-main.640c6f50.js"
+  },
+  {
+    "revision": "d4e223d79789240c6e54f1e63b31cc73",
+    "url": "/Pick-me-ups/static/media/quotation-mark.d4e223d7.svg"
+  }
+]);
